acc14cc2b2b5bdd4b839097f98cdc8fe17508732
XFarooqi <arfarooqix@gmail.com>
2021-12-07 23:52:36 +0500

    Build Guess_Number Game

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
+import random
 
-print("Hello World!")
+lower_bound = int(input("Enter a Lower Number"))
+upper_bound =  int(input("Enter a Upper Bound Number"))
+
+chances = 3
+guess_number = random.randint(lower_bound, upper_bound)
+count = chances
+
+print("Your Number of Chances are:", chances)
+try:
+    for i in range(chances):
+        number = int(input("Enter a Numebr to be guessed!"))
+
+        if number == guess_number:
+            print("Congratulation, you Guessed it Right!!!!")
+            break
+        else:
+            print("Wrong Guess :)")
+            count -= 1
+            print("Remaining Chances are", count)
+except:
+    print("Error Occurs")
